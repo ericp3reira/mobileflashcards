aa226db553d85b53bfff93f6bffb8384d3d21e1a
Eric Pereira <ericmp@gmail.com>
2020-04-28 00:49:47 +0100

    Init project

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+};
---
+++ src/views/Deck.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import {View} from 'react-native';
+
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+
+// import { Container } from './styles';
+
+const Deck = () => <View />;
+
+const mapStateToProps = state => ({});
+
+// const mapDispatchToProps = dispatch =>
+//   bindActionCreators(Actions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  // mapDispatchToProps
+)(Deck);
